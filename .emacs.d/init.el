94c925fcbce03ab545ddf6097d08f73c9649f7f9
Francesco Scali <francesco.scali@gmail.com>
2013-07-07 10:13:58 +0200

    autocomplete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -91,6 +91,13 @@
               "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
+;; ---------------------------
+;; ----------autocomplete-----
+;; ---------------------------
+(add-to-list 'load-path "~/.emacs.d/plugins/auto-complete-1.3.1")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete-1.3.1/dict")
+(ac-config-default)
 
 ;; ----------------------------
 ;; ---------- python ----------
