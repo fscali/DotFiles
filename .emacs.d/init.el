396a9284762d5b0dcd03b97637e78608a23b330b
Francesco Scali <francesco.scali@gmail.com>
2013-07-12 17:58:09 +0000

    orgmode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,3 +109,12 @@
 (add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
 (set-face-background 'highlight-indentation-face "#e3e3d3")
 (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+
+;; ----------------------------
+;; ---------- orgmode ----------
+;; ----------------------------
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
