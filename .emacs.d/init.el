861cf725bdfd04eed6a7491fb60c610548bb1a84
Francesco Scali <francesco.scali@gmail.com>
2013-07-15 16:40:57 +0000

    multi web mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -111,6 +111,17 @@
 (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
 
 ;; ----------------------------
+;; ---------- multi web mode--
+;; ----------------------------
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
+;; ----------------------------
 ;; ---------- orgmode ----------
 ;; ----------------------------
 (require 'org-install)
